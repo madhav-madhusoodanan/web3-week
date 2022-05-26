fda7ca53603b7356487fa6f5a804983f9c578237
Madhav Madhusoodanan <f20200049@pilani.bits-pilani.ac.in>
2022-05-26 06:13:20 +0530

    sponsors

--- src/components/sponsors.jsx
+++ src/components/sponsors.jsx
@@ -1,6 +1,6 @@
 export const Sponsors = () => (
     <div className="w-full h-min z-30">
-        <div className="w-5/6 mx-auto h-full flex flex-row justify-between bg-green-500">
+        <div className="w-5/6 mx-auto h-full flex flex-row flex-wrap justify-between align-middle bg-green-500">
             <div className="w-64 h-64 grid place-items-center relative bg-green-500">
                 <img src="/src/arcana.svg" height={100} width={100}  className="absolute" />
                 <img src="/src/hackathon.svg"/>
