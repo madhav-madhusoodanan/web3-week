0c0945a54638fc02a6d37cd6899d7fac65cfbb0e
Madhav Madhusoodanan <f20200049@pilani.bits-pilani.ac.in>
2022-06-01 19:00:09 +0530

    sponsorzzzzzz

--- src/components/sponsors.jsx
+++ src/components/sponsors.jsx
@@ -1,34 +1,34 @@
 export const Sponsors = () => (
     <div className="w-full min-h-[40vh] z-30">
         <div className="w-5/6 mx-auto h-full flex flex-row flex-wrap justify-between align-middle gap-8">
-            <div className="w-full h-min text-[#79f8bb] font-bold text-3xl md:text-5xl text-center mt-12">
+            <div className="w-full h-min text-[#79f8bb] font-bold text-3xl md:text-4xl text-center mt-12">
                 TITLE SPONSORS
             </div>
-            <div className="w-80  flex-grow mx-auto h-max  grid place-items-center">
+            <div className="w-80 my-4  flex-grow mx-auto h-max  grid place-items-center">
                 <a
-                    src="/src/superblock.svg"
+                    src="/src/superblock.png"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    <img src="/src/superblock.svg" className="h-32" />
+                    <img src="/src/superblock.png" className="h-32" />
                 </a>
             </div>
-            <div className="w-full h-max text-[#79f8bb] font-bold text-3xl md:text-5xl text-center mt-12">
+            <div className="w-full h-max text-[#79f8bb] font-bold text-3xl md:text-4xl text-center mt-12">
                 IN COLLABORATION WITH
             </div>
-            <div className="w-80  flex-grow mx-auto h-max  grid place-items-center relative">
-                <img src="/src/bdt.png" />
+            <div className="w-80 my-4  flex-grow mx-auto h-max  grid place-items-center relative">
                 <a
                     href="https://buidlerstribe.com/"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="absolute top-1 "
-                ></a>
+                >
+                    <img src="/src/bdt.png" />
+                </a>
             </div>
-            <div className="w-full h-max text-[#79f8bb] font-bold text-3xl md:text-5xl text-center mt-12">
+            <div className="w-full h-max text-[#79f8bb] font-bold text-3xl md:text-4xl text-center mt-12">
                 HACKATHON PARTNERS
             </div>
-            <div className="w-80  h-max my-auto grid place-items-center relative mx-8">
+            <div className="w-80 my-4  h-max  grid place-items-center relative mx-auto">
                 <a
                     href="https://devfolio.co/home/"
                     target="_blank"
@@ -37,7 +37,7 @@ export const Sponsors = () => (
                     <img src="/src/devfolio.png" />
                 </a>
             </div>
-            <div className="w-80  mx-8 h-max my-auto  grid place-items-center relative">
+            <div className="w-80 my-4  mx-auto h-max   grid place-items-center relative">
                 <a
                     href="https://polygon.technology/"
                     target="_blank"
@@ -46,7 +46,7 @@ export const Sponsors = () => (
                     <img src="/src/polygon.png" />
                 </a>
             </div>
-            <div className="w-80 h-max  grid place-items-center relative">
+            <div className="w-80 my-4 h-max mx-auto grid place-items-center relative">
                 <a
                     href="https://filecoin.io/"
                     target="_blank"
@@ -55,25 +55,25 @@ export const Sponsors = () => (
                     <img src="/src/filecoin.png" />
                 </a>
             </div>
-            <div className="w-80  h-max  flex flex-col justify-center mx-8 relative ">
+            <div className="w-80 my-4  h-max  flex flex-col justify-center mx-auto relative ">
                 <a
                     href="https://tezos.com/"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    <img src="/src/tezos.png"  className="w-80 "/>
+                    <img src="/src/tezos.png" className="w-80 my-4 " />
                 </a>
             </div>
-            <div className="w-80 h-max  flex flex-col justify-center mx-auto">
+            <div className="w-80 my-4 h-max  flex flex-col justify-center mx-auto">
                 <a
                     href="https://celo.org/"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    <img src="/src/celo.png" className="w-full "/>
+                    <img src="/src/celo.png" className="w-full " />
                 </a>
             </div>
-            <div className="w-80  h-max flex flex-col justify-center mx-auto my-auto">
+            <div className="w-80 my-4  h-max flex flex-col justify-center mx-auto ">
                 <a
                     href="https://www.biconomy.io/"
                     target="_blank"
@@ -90,7 +90,7 @@ export const Sponsors = () => (
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    <div className="w-80  flex-grow mx-auto h-min  grid place-items-center relative">
+                    <div className="w-80 my-4  flex-grow mx-auto h-min  grid place-items-center relative">
                         <img src="/src/arcana.svg" className="w-full" />
                     </div>
                 </a>
